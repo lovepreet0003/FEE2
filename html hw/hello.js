0236872ed132792da8d8b77671f7448888d60be2
lovepreet0003 <lovepreet03340@gmail.com>
2024-06-28 15:16:36 +0530

    heyyy first commit

---
+++ html hw/hello.js
@@ -0,0 +1,3 @@
+<script>
+    console.log("heffdbo")
+</script>
---
+++ work.js
@@ -0,0 +1,143 @@
+
+/*
+const student={
+    name:"lovepreet",
+    age :19,
+    cgpa :9.8,
+    ispass :true
+}
+console.log(student["name"])
+const product={
+    name:"ball Pen",
+    ratings:4,
+    offer:20,
+    price:250
+
+};
+console.log(typeof product)
+const proFile={
+    name:"lovepreet",
+    posts:145,
+    followers:45,
+    following:63,
+    threads:true,
+    follow:false
+    
+}
+console.log([typeof proFile["posts"]])
+let a="lovepreet"
+let b=12
+console.log(a+b)
+console.log(typeof (a+b))
+
+const a1={
+    name:"lovepreet",
+    age:19
+
+}
+a1["adress"]="vpo kot"
+let x=12*/
+/*let a=12
+console.log(a++)
+console.log(a)
+console.log(++a)
+/*const y=13
+console.log(++y)*/
+/*console.log(a+=2)
+ a=6
+let y=7
+console.log(a<y)
+a=6
+y="6"
+console.log(a===y)
+let w=5;
+let z=6;
+console.log(w<z && w==5)
+console.log(w<z || w!=5)*/
+
+/*let a=prompt("what is your age")
+a=Number.parseInt(a)
+if (a<0){
+    alert("invalid age")
+}
+else if (a>0 && a<10){ 
+    alert("u cant drive")
+}
+else if (a>10 && a<18){ 
+    alert("u can think")
+}
+else{
+    alert("u can drive")
+}
+let marks=11
+console.log((marks>10)? "yes":"no")*/
+/*let age=prompt("enter the age ")
+if (age>10 && age<20){
+    console.log("yes")
+}
+else{console.log("no")}*/
+/*let age= prompt("enter your age")
+switch(age){
+    case '12':
+        console.log("12")
+    case '13':
+        console.log("13")
+    case '14':
+        console.log("14")
+    default:
+        console.log("invalid")
+        
+}*/
+/*let a=prompt("enter the number")
+a=Number.parseInt(a)
+if (a%2==0 || a%3==0){
+    console.log("a is divisible by 2 or 3")
+}
+else{
+    console.log("no")
+}*/
+/*let age=prompt("enter the age")
+console.log((age>=18)? "drive":"cannot drive")*/
+/*let fact=1
+let n=prompt("enetr the n")
+for (let i=1; i<=n; i++){
+    fact*=i
+}console.log(fact)*/
+/*let obj={
+
+
+    luv:90,
+    shiv:89,
+    rohan:70
+}
+for (let a in obj){
+    console.log("marks of" + a + "are" + obj[a])
+}*/
+/*function myfunc(a,b){
+    return console.log((a+b)/2)
+}
+myfunc(5,5)
+const sum=(x,y)=>{
+    let z=x+y;
+    return z
+}
+console.log(sum(5,5))*/
+/*let obj={
+    harry:98,
+    luv:99,
+    akash:7
+}
+for(let i=0; i<Object.keys(obj).length; i++){
+    marks="marks of"+Object.keys(obj)[i] + "are" + obj[Object.keys(obj)[i]]
+    console.log(marks)
+}*/
+/*let cn=3
+let i
+while (cn!=i){
+i=prompt("enter the number")
+}
+console.log(" u hv entrd a crct no")*/
+function mean(a,b,c,d,e){
+    return console.log((a+b+c+d+e)/5)
+}
+mean(1,2,3,3,5)
